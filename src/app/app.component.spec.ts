121776f5fd2fe2c0b0c6a15fc16583ff4c5178b7
PurityKomen <jepkorirpurity8@gmail.com>
2024-12-20 15:11:58 +0300

    list contacts on the table with name and phonenumber

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'phonebook' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('phonebook');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, phonebook');
-  });
 });
--- src/components/contactlist/contactlist.component.ts
+++ src/components/contactlist/contactlist.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../contact.service';
+import { Contact } from '../contact';
 
 @Component({
   selector: 'app-contactlist',
   standalone: true,
-  imports: [],
+  imports: [ ],
+  providers: [ContactService],
   templateUrl: './contactlist.component.html',
   styleUrl: './contactlist.component.css'
 })
-export class ContactlistComponent {
+export class ContactlistComponent implements OnInit{
+  constructor(private contactService:ContactService) {}
+  contactList!: Contact[];
+
+  ngOnInit() {
+    this.contactService.getContacts().subscribe(data => {
+      this.contactList = data.sort((a,b) => a.name > b.name ? 1 : -1)
+      console.log('contact',this.contactList)
+    }) 
+  }
 
 }
